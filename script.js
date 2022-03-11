cf81509325f3390cde94801bcefb0916079b7571
Glitch (glitch-hello-website) <none>
2022-03-11 02:13:23 +0000

    🐀🌘 Checkpoint ./script.js:41298979/1:41311861/431:42995283/228 ./style.css:42995347/182 ./.glitch-assets:42995347/373 ./index.html:42995347/60

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
+// this line of code is read first and input = empty/undefine variable has no value
 
-let input = document.querySelectorAll("input[type=text]");
 
-// User enters info and hits submit
-document.querySelector("#submit").addEventListener("click", addUserListToPage);
+// User enters info and hits submit this is run
+
 
 
 // determines that the user has entered data
 function userInputLength(){
-  
-  return input.length
-  }
+  document.querySelector("#submit").addEventListener("click", addUserListToPage);
+  let userInput = document.querySelectorAll("input[type=text]");
+  return userInput.length
+}
 
 // When user has entered info, will create list      
   function addUserListToPage(){
-    if (userInputLength > 0) {
+    if (userInputLength() > 0) {
       createUserInputList()
+      }
     }
-  }
 
 // Creates list of user  
 function createUserInputList(list){
     let text = "";
-    
-
+    let input = document.querySelectorAll("input[type=text]").value;
     for (let i = 0; i < input.length; i++) {
         text += input[i].value;
     }
-  let li = document.createElement("li");
+    let li = document.createElement("li");
     let node = document.createTextNode(text);
     li.appendChild(node);
     document.getElementById("list").appendChild(li);
-}
+  }
+
