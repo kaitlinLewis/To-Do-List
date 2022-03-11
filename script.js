fad9d53572605cfbba5ea133806993e3ef6b8eeb
Glitch (glitch-hello-website) <none>
2022-03-11 01:02:21 +0000

    🔖🌶 Checkpoint ./index.html:42995280/61:42995283/325:42995347/2 ./script.js:41298979/1286

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function () {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
-}
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+function myFunction(list){
+    var text = "";
+    var inputs = document.querySelectorAll("input[type=text]");
+    for (var i = 0; i < inputs.length; i++) {
+        text += inputs[i].value;
+    }
+    var li = document.createElement("li");
+    var node = document.createTextNode(text);
+    li.appendChild(node);
+    document.getElementById("list").appendChild(li);
