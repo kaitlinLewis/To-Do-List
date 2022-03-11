0a673a34fb6087dba0c6701b19007e9df7e44b16
Glitch (glitch-hello-website) <none>
2022-03-11 18:41:19 -0500

    to do list with Videet and Tony

--- main.js
+++
@@ -1,9 +0,0 @@
-function mainFunction(list) {
-  let text = "";
-  let inputs = document.querySelectorAll("input[type=text]");
-  for (let i = 0; i < inputs.length; i++) {
-    text += inputs[i].value;
-  }
-  
-  
-}
--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 //Get length of user input
-function userInputLength(){
+function userInputLength() {
   return document.getElementById("userText").value.length;
 }
 
-// Creates list of user text 
-function createUserInputList(){
+// Creates list of user text
+function createUserInputList() {
   let li = document.createElement("li");
-  li.appendChild(document.createTextNode(document.getElementById("userText").value));
+  li.appendChild(
+    document.createTextNode(document.getElementById("userText").value)
+  );
   document.querySelector("ul").appendChild(li);
 }
 
 // Adds user input to list
-function addUserTextToList(){
+function addUserTextToList() {
   if (userInputLength() > 0) {
-      createUserInputList()
-      }
+    createUserInputList();
+  }
 }
 
 document.querySelector("#submit").addEventListener("click", addUserTextToList);
-
-// Hey guys this is videet lets complete this whenever you get time. feel free to edit and fiddle around.
-// Update: it works!!!!! Now to just get it to cross off done tasks...
-
-//Tony (March 11th @ 6PM CDT): Nice work! Should we add media query for responsive design on mobile? 
-// maybe collapse or align completed tasks? - Tony
