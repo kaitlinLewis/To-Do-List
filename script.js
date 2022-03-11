86534b3d24b663c7e540b24a2b3931bbfdd93d75
Glitch (glitch-hello-website) <none>
2022-03-11 01:14:25 +0000

    🏌🍶 Checkpoint ./index.html:41298979/79:42995283/34 ./script.js:41298979/26:42995280/2:42995283/174

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
 
 
-function myFunction(list){
-    var text = "";
-    var inputs = document.querySelectorAll("input[type=text]");
-    for (var i = 0; i < inputs.length; i++) {
+function createUserInputList(list){
+    let text = "";
+    let inputs = document.querySelectorAll("input[type=text]");
+    for (let i = 0; i < inputs.length; i++) {
         text += inputs[i].value;
     }
-    var li = document.createElement("li");
-    var node = document.createTextNode(text);
+    let li = document.createElement("li");
+    let node = document.createTextNode(text);
     li.appendChild(node);
     document.getElementById("list").appendChild(li);
+  let userInputLength = 
+      
+  function addUserListToPage(){
+    if userInputLength > 0 {
+      
+    }
+    
+  }
